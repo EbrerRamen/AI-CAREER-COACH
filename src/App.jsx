4a264615ce5f289ac23a099f40979102a4541160
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-10-14 02:29:05 +0600

    Fixed Enlist error. (BrowserRouter was not imported properly)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import ResumeUpload from "./pages/ResumeUpload";
 import Result from "./pages/Result";
