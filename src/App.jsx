9ce5424a4ccc0e7b58f9421b5c0d74e0853c5e8e
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-10-14 02:19:40 +0600

    Removed styling from main layout for now

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,15 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen flex flex-col bg-gray-50">
+    <BrowserRouter>
         <Navbar />
-        <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/upload" element={<ResumeUpload />} />
             <Route path="/result" element={<Result />} />
           </Routes>
-        </main>
         <Footer />
-      </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
