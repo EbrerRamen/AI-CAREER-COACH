299cec7b3ad75ac49c40f77a9ab9c0870b381ccd
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2025-10-25 21:37:54 +0600

    CORS updated (Using vercel link)

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,8 @@ from dotenv import load_dotenv
 from openai import OpenAI
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["ai-career-coach-one-rho.vercel.app"])
+# CORS(app)
 
 load_dotenv()
 
